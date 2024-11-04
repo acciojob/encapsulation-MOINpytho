36cb3987b5d583bf64a7a96f07c96252bbb3e311
Moin <53350043+MOINpytho@users.noreply.github.com>
2024-11-04 20:18:17 +0530

    Update Main.java

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,12 @@
 package com.driver;
 
 public class Main {
-  
+  public static void main(String[] args) {
+          RWOnly r = new RWOnly();
+//        r.name = "asd";
+//        System.out.print(r.name);
+//        java: name has private access in com.driver.RWOnly
+          r.setName("me");
+          System.out.println(r.getName());
+    }
 }
